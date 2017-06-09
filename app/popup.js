0e41daaffe975b46fcc45fd7fe570fa9456dc19f
antmo646 <antmo646@student.liu.se>
2017-06-09 03:15:59 +0200

    first Commit

---
+++ app/popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  //var bookingBtn = $("#setBookingBtn");
+  var bookingBtn = document.getElementById("setBookingBtn");
+  console.log("TJAAAGGA")
+  bookingBtn.addEventListener('click', cdBooking);
+  $('#mdp-demo').multiDatesPicker();
+})
